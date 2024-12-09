a726c7bea0bcdfdc4e2fe95a16e7d9f318d176ed
YGK3333 <anirudhkosa@gmail.com>
2024-12-09 15:59:27 +0100

    adding new project

---
+++ src/samplePackage/SumofNumbers.java
@@ -0,0 +1,17 @@
+package samplePackage;
+
+public class SumofNumbers {
+
+	public static void main(String[] args) {
+		int a = 0;
+
+		for (int i = 1; i <= 10; i++) {
+
+			a += i;
+
+		}
+		System.out.println(a);
+
+	}
+
+}
